1da098daafaf7f86a93e8dbc0abc37dad103a42a
jhnnsrs <jhnnsrs@gmail.com>
2024-06-20 23:49:08 +0200

    docs

--- akuire/managers/base.py
+++ akuire/managers/base.py
@@ -12,4 +12,15 @@ class Manager(Protocol):
         self, event: ManagerEvent
     ) -> AsyncGenerator[DataEvent, None]: ...
 
-    def challenge(self, event: ManagerEvent) -> bool: ...
+    def challenge(self, event: ManagerEvent) -> bool:
+        """Check if the manager can handle the event
+
+        The challenge method is used to check if the manager can handle a specific event. This method should return True
+        if the manager can handle the event and False otherwise.
+
+        The AcquistionEngine will use this method to determine if the manager can handle the event. If the manager can
+        handle the event, the compute_event method will be called to execute the event.
+
+
+        """
+        ...

1aa16f0677c69c38595c441dfaf16cfa5dc45619
jhnnsrs <jhnnsrs@gmail.com>
2024-06-20 22:20:21 +0200

    xx

--- akuire/events/data_event.py
+++ akuire/events/data_event.py
@@ -5,11 +5,22 @@ import numpy as np
 
 @dataclasses.dataclass
 class DataEvent:
+    """Base class for all data events
+
+    Data events are used to communicate data that are
+    generated during the acquisition process. This
+    can be images, metadata, or other data that is
+    generated during the acquisition process.
+
+
+    """
+
     pass
 
 
 @dataclasses.dataclass
 class ImageDataEvent(DataEvent):
+
     data: np.ndarray
     """Data should be 5 Dimensional c,t,z,y,x"""
 
